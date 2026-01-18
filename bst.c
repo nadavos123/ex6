a5c314e842a6b52ea4c304998400794f6c8b4b09
nadavos123 <nadavos0102@gmail.com>
2026-01-18 23:05:21 +0200

    Updated the dungeon crawler logic

--- bst.c
+++ bst.c
@@ -12,23 +12,28 @@ BST* createBST(int (*cmp)(void*, void*), void (*print)(void*), void (*freeData)(
     tree->freeData=freeData;
     tree->root=NULL;
 }
-BSTNode* bstInsert(BSTNode* root, void* data, int (*cmp)(void*, void*)){
-    if(root=NULL){                    
-        root=malloc(sizeof(BSTNode));
-        root->data=data;
-        root->right=NULL;
-        root->left=NULL;
-        return root;
-    }
-    if(root->left==NULL&&!cmp(data, root->data)){
-        return(bstInsert(root->left, data, cmp));
-    }
-    return(bstInsert(root->right, data, cmp));
-    
-}
-BST* createBST(int (*cmp)(void*, void*), void (*print)(void*), void (*freeData)(void*)){
 
+BSTNode* bstInsert(BSTNode* root, void* data, int (*cmp)(void*, void*)){
+    if(root==NULL){
+        return -1;
+    }
+    if(cmp(root->data,data)){
+        if(root->left!=NULL){
+            return bstInsert(root->left,data,cmp);
+        }
+        root->left=createBST;
+        return 0;
+    }
+    if(!cmp(root->data,data)){
+        if(root->right!=NULL){
+            return bstInsert(root->right,data,cmp);
+        }
+        root->right=createBST;
+        return 0;
+    }
+    return 0;
 }
+
 int compareInt(int *a, int *b){
     if(*a>*b){
         return 1;
@@ -42,8 +47,5 @@ int compareChar(char *a, char *b){
     return 0;
 }
 int compareString(char *a, char *b){
-    if(strcmp(a,b)>0){
-        return 1;
-    }
-    return 0;
+    return(strcmp(a,b)>0);
 }    
